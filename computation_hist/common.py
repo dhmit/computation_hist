8e2d9a5719857449ebcf5314557e6ba21135540d
Stephan Risi <stephan.risi@gmail.com>
2018-11-15 15:37:40 -0500

    Added gsheet downloader and document class
    Also added setup and requirements

---
+++ computation_hist/common.py
@@ -0,0 +1,114 @@
+import gzip
+import os
+import pickle
+from pathlib import Path
+
+import pandas as pd
+
+
+BASE_PATH = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+def store_pickle(obj, filename):
+    """
+    Store a compressed "pickle" of the object in the "pickle_data" directory
+    and return the full path to it.
+
+    The filename should not contain a directory or suffix.
+
+    Example in lieu of Doctest to avoid writing out a file.
+
+        my_object = {'a': 4, 'b': 5, 'c': [1, 2, 3]}
+        gender_novels.common.store_pickle(my_object, 'example_pickle')
+
+    :param obj: Any Python object to be pickled
+    :param filename: str | Path
+    :return: Path
+    """
+    filename = BASE_PATH / 'data' / 'pickle_data' / (str(filename) + '.pgz')
+    with gzip.GzipFile(filename, 'w') as fileout:
+        pickle.dump(obj, fileout)
+    return filename
+
+
+def load_pickle(filename):
+    """
+    Load the pickle stored at filename where filename does not contain a
+    directory or suffix.
+
+    Example in lieu of Doctest to avoid writing out a file.
+
+        my_object = gender_novels.common.load_pickle('example_pickle')
+        my_object
+        {'a': 4, 'b': 5, 'c': [1, 2, 3]}
+
+    :param filename: str | Path
+    :return: object
+    """
+    filename = BASE_PATH / 'data' / 'pickle_data' / (str(filename) + '.pgz')
+    with gzip.GzipFile(filename, 'r') as filein:
+        obj = pickle.load(filein)
+    return obj
+
+
+def get_google_sheet(spreadsheet_id, sheet_name, return_type='dataframe'):
+    """
+    Loads a google sheet by the spreadsheet_id and the sheet name
+    It will be primarily used to load the metadata from google sheets
+
+    Note: To run this function, you need to have copied the google_credentials.json from the
+    general slack channel into the google_credentials folder. When you run the script for the
+    first time, it will create a token.json in the google_credentials folder that is specific to
+    you.
+
+    Returns either a pandas dataframe...
+    >>> df = get_google_sheet('1LU05c0lTSTQ9IY3RS4eDcyvq5HNBMx6mbuMJe6TX-ZA', '2_verzuh')
+    >>> df['author'][0]
+    'Corbato, F. J.'
+
+    ... or a list of dicts
+    >>> list_of_dicts = get_google_sheet('1LU05c0lTSTQ9IY3RS4eDcyvq5HNBMx6mbuMJe6TX-ZA',
+    ...                                  '2_verzuh', return_type='list_of_dicts')
+    >>> list_of_dicts[0]['author'], list_of_dicts[0]['title']
+    ('Corbato, F. J.', 'Requisition for materials for Audio Monitor of 704 Computer')
+
+    """
+
+    from apiclient.discovery import build
+    from httplib2 import Http
+    from oauth2client import file, client, tools
+
+    if return_type not in ['dataframe', 'list_of_dicts']:
+        raise ValueError('return_type has to be (pandas) "dataframe" or "list_of_dicts" but not '
+                         '{return_type}.')
+
+    scopes = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+    # Setup the Sheets API
+    store = file.Storage(Path(BASE_PATH, 'data', 'google_credentials', 'token.json'))
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets(Path(BASE_PATH, 'data', 'google_credentials',
+                                                   'google_credentials.json'), scopes)
+        creds = tools.run_flow(flow, store)
+    service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+    # Call the Sheets API
+    gsheet = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id,
+                                                 range=sheet_name).execute()
+
+    values = gsheet.get('values', [])
+    df = pd.DataFrame(values)
+    # Turn first row into header
+    df = df.rename(columns=df.iloc[0]).drop(df.index[0])
+    df = df.reset_index()
+
+    if return_type == 'dataframe':
+        return df
+    elif return_type == 'list_of_dicts':
+        return df.to_dict('records')
+
+
+if __name__ == '__main__':
+    from dh_testers.testRunner import main_test
+    main_test(import_plus_relative=True)  # this allows for relative calls in the import.
+
---
+++ computation_hist/document.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+from computation_hist.common import BASE_PATH
+
+
+class Document():
+    """
+    The document loads and holds the metadata and text fore one archival document
+    Currently, it is only a stub for further development
+
+    TODO: This may not be needed as a class but might get ported to a Django model.
+
+    You can load a sample document including ocred text with
+    >>> sample_doc = Document(load_sample_doc=True)
+    >>> sample_doc.author, sample_doc.title
+    ('Corbato, F. J.', 'Requisition for materials for Audio Monitor of 704 Computer')
+
+    """
+
+    def __init__(self, document_metadata_dict=None, load_sample_doc=False):
+        """
+        Loads the metadata for a document
+
+        :param document_metadata_dict: dict or None
+        :param load_sample_doc: True to load the sample document. Default: False
+        :param load_text: True to load the text, False skips loading the text. Default: True
+
+        Note: You can set the document text by passing a 'text' key
+        >>> metadata = {'box': 2, 'foldername_short': 'verzuh', 'doc_id': 1,
+        ...             'filename': '2_verzuh_1', 'author': 'Corbato, F. J.', 'title': 'n/a',
+        ...             'date': '1957-11-21', 'text': 'Sample text...'}
+        >>> doc = Document(metadata)
+        >>> doc.text
+        'Sample text...'
+
+        You can also load a sample document without passing a document_metadata_dict
+        >>> sample_doc = Document(load_sample_doc=True)
+        >>> sample_doc.author, sample_doc.title
+        ('Corbato, F. J.', 'Requisition for materials for Audio Monitor of 704 Computer')
+
+        The sample_doc includes messy ocr text
+        >>> sample_doc.text[8:68].split()
+        ['F.', 'J.', 'Corbato', 'M.', 'November', '21,', '1957', 'Verzuh', 'To:', 'Dr.', 'Subject:']
+
+        """
+
+        # If we load the sample doc, set the metadata_dict to the sample_doc
+        if load_sample_doc:
+            if document_metadata_dict:
+                print("WARNING: Loading sample document but also received a "
+                      "document_metadata_dict. If you want to load that data, set load_sample_doc "
+                      "to False.")
+            else:
+                document_metadata_dict = {
+                    'box': 2, 'foldername_short': 'verzuh', 'doc_id': 1, 'filename': '2_verzuh_1',
+                    'foldername_full': 'Verzuh, Dr. Frank M.', 'author': 'Corbato, F. J.',
+                    'title': 'Requisition for materials for Audio Monitor of 704 Computer',
+                    'date': '1957-11-21', 'type': 'letter', 'recipients': 'Verzuh, F. M.',
+                    'notes': 'None',
+                    'pdf_path': Path(BASE_PATH, 'data', 'sample_docs', '2_verzuh_1.pdf'),
+                    'txt_path': Path(BASE_PATH, 'data', 'sample_docs', '2_verzuh_1.txt'),
+                }
+
+        if not hasattr(document_metadata_dict, 'items'):
+            raise ValueError(
+                'document_metadata_dict must be a dictionary or support .items()')
+
+        # Check that the essential attributes for the document exists.
+        # TODO: Decide which attributes are essential for the document
+        for key in ('box', 'foldername_short', 'doc_id', 'filename', 'author', 'title',
+                    'date'):
+            if key not in document_metadata_dict:
+                raise ValueError(f'document_metadata_dict must have an entry for "{key}". Full ',
+                                 f'metadata: {document_metadata_dict}')
+
+        # TODO: Check date format (YYYY-MM-DD)
+
+        for key in document_metadata_dict:
+            setattr(self, key, document_metadata_dict[key])
+
+        # Load text if it wasn't passed as a metadata key
+        if not hasattr(self, 'text'):
+            # Check that the filename looks like a filename (ends in .txt)
+            if not self.txt_path.parts[-1].endswith('.txt'):
+                raise ValueError(
+                    f'The document txt_path({self.txt_path}) should end in .txt . Full '
+                    f'metadata: {document_metadata_dict}.')
+            try:
+                with open(self.txt_path) as f:
+                    self.text = f.read()
+            except FileNotFoundError:
+                err = "Could not find the novel text file "
+                err += "at the expected location ({file_path})."
+                raise FileNotFoundError(err)
