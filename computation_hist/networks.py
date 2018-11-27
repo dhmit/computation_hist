5efd8ed78ef918ca4f8523ccfc1f8676607285fa
Stephan Risi <stephan.risi@gmail.com>
2018-11-27 16:07:33 -0500

    Update computation_hist/networks.py

--- computation_hist/networks.py
+++ computation_hist/networks.py
@@ -74,6 +74,110 @@ def create_network_graph_weighted():
     plt.show()
 
 
+
+
+class Network():
+
+    def __init__(self):
+
+        self.nodes = {}
+        self.edges = {}
+
+        df = get_metadata()
+        for document_metadata in df.iterrows():
+
+            document_metadata = document_metadata[1]
+
+            document = Document(document_metadata)
+
+            # add the new document to the author node
+            print(document.author)
+            if document.author not in self.nodes:
+                self.nodes[document.author] = Node(document.author)
+            self.nodes[document.author].add_document(document)
+
+            if document_metadata['recipients'] not in [None, 'None']:
+                self._add_edge(document)
+
+    def _add_edge(self, document):
+
+        author = document.author
+        recipient = document.recipients
+        for name in [author, recipient]:
+            if not name in self.nodes:
+                self.nodes[name] = Node(name)
+        if not (author, recipient) in self.edges:
+            self.edges[(author, recipient)] = Edge(author, recipient)
+        self.edges[(author, recipient)].add_document(document)
+
+
+class Node():
+
+    def __init__(self, name):
+        self.name = name
+        self.documents = []
+
+    def __repr__(self):
+        return f"Node: {self.name}"
+
+    def __eq__(self, other):
+
+        if self.name == other.name:
+            return True
+        else:
+            return False
+
+    def __len__(self):
+        return len(self.documents)
+
+    def add_document(self, document):
+        self.documents.append(document)
+
+
+class Edge():
+
+    def __init__(self, author, recipient):
+        self.author = author
+        self.recipient = recipient
+        self.documents = []
+
+    def __repr__(self):
+        return f"Edge. Author: {self.author}. Recipient: {self.recipient}"
+
+    def __eq__(self, other):
+
+        if (self.author == other.author and self.recipient == other.recipient) or \
+           (self.author == other.recipient and self.recipient == other.author):
+            return True
+        else:
+            return False
+
+    def __len__(self):
+        return len(self.documents)
+
+    def add_document(self, document):
+        self.documents.append(document)
+
+
+
+class Document():
+
+    def __init__(self, document_metadata):
+        self.date = document_metadata['date']
+        self.doc_type = document_metadata['type']
+        self.author = document_metadata['author']
+        self.title = document_metadata['title']
+        self.filename = document_metadata['filename']
+
+        if document_metadata['recipients'] == 'None':
+            self.recipients = None
+        else:
+            self.recipients = document_metadata['recipients']
+
+
+
+
+
 if __name__ == '__main__':
 
     create_network_graph_weighted()
