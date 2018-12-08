0be414d3e39d89a3c566b4377ed5d57b19af465e
Michael Scott Cuthbert <cuthbert@mit.edu>
2018-12-08 11:20:40 -0500

    update django and gitignore

--- djweb/dj_comp_hist/views.py
+++ djweb/dj_comp_hist/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Person
+
 
 # Create your views here.
 
@@ -8,8 +10,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at Computation History project.")
 
+
 def person(request, person_id):
     person_obj = get_object_or_404(Person, pk=person_id)
-    response = f"You're looking at {person_obj.first} {person_obj.last}"
-    return HttpResponse(response)
+    # response = f"You're looking at {person_obj.first} {person_obj.last}"
+    return render(request, 'person.html', {'person_obj': person_obj})
+    # return HttpResponse(response)
 
+
+
+def document(request, document_id):
+    pass
