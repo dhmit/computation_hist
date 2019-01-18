cbd9ac700ab574185d5b8273a19c5de8032b7909
Elena Boal <eboal@mit.edu>
2019-01-18 14:56:14 -0500

    Made some attributes not required
    Decided which attributes were actually necessary, made string version of document less redundant

--- djweb/dj_comp_hist/models.py
+++ djweb/dj_comp_hist/models.py
@@ -5,7 +5,7 @@ import csv
 
 
 class Organization(models.Model):
-    location = models.CharField(max_length=191)
+    location = models.CharField(max_length=191, blank=True)
 
 
 class Box(models.Model):
@@ -13,9 +13,9 @@ class Box(models.Model):
 
 
 class Person(models.Model):
-    first = models.CharField(max_length=191)
-    last = models.CharField(max_length=191)
-    organization = models.ManyToManyField(Organization)
+    first = models.CharField(max_length=191, blank=True)
+    last = models.CharField(max_length=191, blank=True)
+    organization = models.ManyToManyField(Organization, blank=True)
 
     def __str__(self):
         if self.last and self.first:
@@ -32,26 +32,29 @@ class Folder(models.Model):
     name = models.CharField(max_length=191)
     box = models.ForeignKey(Box, on_delete=models.CASCADE)
     full = models.CharField(max_length=191)
+    number = models.IntegerField(default=0)
 
 
 class Document(models.Model):
-    author = models.ManyToManyField(Person, related_name='author')
+    author_person = models.ManyToManyField(Person, related_name='author_person', blank=True)
+    author_organization = models.ManyToManyField(Organization,
+                                                 related_name='author_organization', blank=True)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
-    title = models.CharField(max_length=191)
-    type = models.CharField(max_length = 191)
+    title = models.CharField(max_length=191, blank=True)
+    type = models.CharField(max_length = 191, blank=True)
     # TODO: turn type into choices- note that choices needs to be able to grow
     number_of_pages = models.IntegerField(default=1)
-    date = models.DateField(auto_now_add=False, auto_now=False)
-    recipient = models.ManyToManyField(Person, related_name='recipient')
+    date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    recipient_person = models.ManyToManyField(Person, related_name='recipient_person', blank=True)
+    recipient_organization = models.ManyToManyField(Organization,
+                                                    related_name='recipient_organization',
+                                                    blank=True)
 
     def __str__(self):
-        if self.title and self.author:
+        if self.title:
             return self.title
-        elif self.title:
-            return self.title
-
         else:
-            return "No title or author"
+            return "No title"
 
 
 class Page(models.Model):
