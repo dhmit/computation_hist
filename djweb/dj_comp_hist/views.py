3adf256be96ebe965799c28aac86076a40b11865
Ife Ademolu-Odeneye <ife.ademoluodeneye@gmail.com>
2019-01-24 14:42:56 -0500

    Update views.py

--- djweb/dj_comp_hist/views.py
+++ djweb/dj_comp_hist/views.py
@@ -14,9 +14,6 @@ def person(request, person_id):
     person_obj = get_object_or_404(Person, pk=person_id)
     document_objs = person_obj.author_person.all()
     x = render(request, 'person.html', {'person_obj': person_obj, 'document_objs': document_objs, 'length': len(document_objs)})
-    print("******************")
-    print(Document)
-
     return x
 
 
