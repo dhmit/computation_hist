e64d43d8a0ea2d9eec0a1f6f76ad67b16ebed33e
Carol Pan <carolpan@mit.edu>
2019-01-24 16:49:56 -0500

    Update urls.py

--- djweb/dj_comp_hist/urls.py
+++ djweb/dj_comp_hist/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #re_path(r'person/(?P<person_id>).*', views.person, name='person'),
     path(r'person/<int:person_id>', views.person, name='person'),
     path('doc/<int:doc_id>', views.doc, name='document'),
     path('box/<int:box_id>', views.box, name='box'),
-    #path('folder/<int:folder_id>', views.folder, name='folder'),
+    path('folder/<int:folder_id>', views.folder, name='folder'),
+    path('org/<int:org_id', views.organization, name='organization'),
 
+    # Todo: Slugify at a much later time?
 ]
