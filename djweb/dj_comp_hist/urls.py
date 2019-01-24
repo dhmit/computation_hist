f0ecfffe4e24942528513443f7fac92ca80199aa
Carol Pan <carolpan@mit.edu>
2019-01-24 16:18:03 -0500

    Update urls.py

--- djweb/dj_comp_hist/urls.py
+++ djweb/dj_comp_hist/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('person/<int:person_id>', views.person, name='person'),
+    #re_path(r'person/(?P<person_id>).*', views.person, name='person'),
+    path(r'person/<int:person_id>', views.person, name='person'),
     path('doc/<int:doc_id>', views.doc, name='document'),
+    path('box/<int:box_id>', views.box, name='box'),
+    #path('folder/<int:folder_id>', views.folder, name='folder'),
+
 ]
