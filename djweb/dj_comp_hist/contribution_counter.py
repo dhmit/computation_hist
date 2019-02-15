8fb56e12bd88776c3d96e5c2828b0de451ddc3c3
Mayowa99 <47461505+Mayowa99@users.noreply.github.com>
2019-02-15 17:27:11 -0500

    Update contribution_counter.py

--- djweb/dj_comp_hist/contribution_counter.py
+++ djweb/dj_comp_hist/contribution_counter.py
@@ -1,13 +1,22 @@
 import csv
 
-number_contributions = {}
+def contribution_counter():
+    """
+    This function returns a list of people who have contributed metadata.
+    It returns a list of tuples where the first element is the person and the
+    last element is the number of contributions. The list is ordered with the
+    person who has contributed the most appearing first.
 
-with open("metadata.csv") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        person = row["metadata_added_by"]
-        if person not in number_contributions.keys():
-            number_contributions[person] = 1
-        else:
-            number_contributions[person] += 1
-print(sorted(number_contributions.items(), key=lambda kv: -kv[1]))
+    :return: list
+    """
+    number_contributions = {}
+
+    with open("metadata.csv") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            person = row["metadata_added_by"]
+            if person not in number_contributions.keys():
+                number_contributions[person] = 1
+            else:
+                number_contributions[person] += 1
+    return sorted(number_contributions.items(), key=lambda kv: -kv[1])
