c395db18a3a9d376f049efdcc2b800506598a0be
samimak37 <41493783+samimak37@users.noreply.github.com>
2019-03-10 14:29:55 -0400

    Update netx.py
    tried to fix the edge bug. Failed, but hopefully this is a start

--- computation_hist/netx.py
+++ computation_hist/netx.py
@@ -51,25 +51,47 @@ def add_doc(graph, doc_meta):
     doc = Document(doc_meta)
     author = doc.author
     recipients = doc.recipients
+    recipients.remove('None')
 
     # Adds author to the graph or increases the weight of the node
     if author not in graph:
         graph.add_node(author, weight=1)
     else:
-        old_weight = graph.nodes(data='weight')[author]
+        old_weight = graph.nodes[author]['weight']
         graph.remove_node(author)
         graph.add_node(author, weight=old_weight + 1)
 
     # Adds edges based on who the letter has been sent to
-    for recip in recipients:
-        if recip not in graph:
-            graph.add_node(recip)
+    if recipients:
+        for recip in recipients:
+            if recip not in graph:
+                graph.add_node(recip, weight=1)
+
+            print('before adding:', graph.edges)
+            print('set:', (author, recip))
+            if (author, recip) not in graph.edges:
+                graph.add_edge(author, recip, weight=1)
+                print('clean add:', graph.edges)
+            else:
+                old_weight = graph.edges[author, recip]['weight']
+                graph.remove_edge(author, recip)
+                print('remove old:', graph.edges)
+                graph.add_edge(author, recip, weight=old_weight + 1)
+                print('add with new weight:', graph.edges)
+    else:
+        if 'None' not in graph:
+            graph.add_node('None', weight=1)
+
+        if (author, 'None') not in graph.edges:
+            graph.add_edge(author, 'None', weight=1)
         else:
-            graph.add_edge(author, recip)
+            old_weight = graph.nodes[author]['None']['weight']
+            graph.remove_edge(author, 'None')
+            graph.add_edge(author, 'None', weight=old_weight + 1)
 
 
 if __name__ == '__main__':
-    graph = make_graph(debug=True)
-    nx.draw(graph, with_labels=True)
+    g = make_graph(debug=True)
+    nx.draw(g, with_labels=True)
     plt.show()
 
