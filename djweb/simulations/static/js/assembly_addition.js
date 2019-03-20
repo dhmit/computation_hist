40e40037ffcf14f89876d021c7ee4bb892379fd6
Mingfei <mingfeid@mit.edu>
2019-03-19 21:43:15 -0400

    Example line

--- djweb/simulations/static/js/assembly_addition.js
+++ djweb/simulations/static/js/assembly_addition.js
@@ -1,6 +1,12 @@
 const computer_size = 7;
 const num_code_lines = 4;
 const highlighted_registers = [6];
+const program = [
+    new Assembly_Line(
+        0, "CLA 4", "<b>CLA 4: </b> Clears the accumulator and stores the value at address 4 (which" +
+        " you'll see if you mouseover is 12) into the accumulator.", ["ac"],
+        )
+];
 
 /**
  * Initializes assembly_addition.jinja2, including initializing register 3 of general memory to
