6d78b4bf139e9e48d8449b4bce6ab90e3787dd09
Mingfei <mingfeid@mit.edu>
2019-04-04 14:09:54 -0400

    TZE operation

--- static/js/simulator.js
+++ static/js/simulator.js
@@ -24,6 +24,7 @@ const no_to_operation_b = {
     0o240: FDH,
     0o241: FDP,
     0o020: TRA,
+    0o100: TZE,
 };
 
 const no_to_operation_a = {
@@ -1631,6 +1632,19 @@ function TRA(computer, address) {
     computer.ilc.update_contents(address);
 }
 
+/**
+ * Emulates the IBM 704 Transfer on Zero (TZE) operation.  If bits Q-35 of the accumulator are 0,
+ * the Instruction Location Counter jumps to the specified address.  Otherwise, the program continues
+ * to the next instruction.
+ *
+ * @param {IBM_704} computer    Machine to execute instruction on.
+ * @param {number}  address     Address to jump to.
+ */
+function TZE(computer, address) {
+    if (computer.accumulator.fixed_point === 0) {
+        computer.ilc.update_contents(address);
+    }
+}
 // Type A operations
 
 /**
