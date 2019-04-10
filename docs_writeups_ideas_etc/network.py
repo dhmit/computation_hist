30cd930b799fc19b6dce53b5596de9eb0736489e
samimak37 <41493783+samimak37@users.noreply.github.com>
2019-04-09 21:25:21 -0400

    Update network.py

--- docs_writeups_ideas_etc/network.py
+++ docs_writeups_ideas_etc/network.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 
 def create_network_json():
-
+    """
     # get all documents
     all_docs = Document.objects.all()
 
@@ -17,4 +17,15 @@ def create_network_json():
         for author in authors:
             for recipient in recipients:
                 edge_counter[(author.fullname, recipient.fullname)] += 1
+    """
 
+    docs = Document.objects.all()
+    nodes = Counter()
+    edges = Counter()
+
+    for doc in docs:
+        for author in doc.author_person.all():
+            nodes[author] += 1
+            for recip in doc.recipient_person.all():
+                edges[(author, recip)] += 1
+                nodes[recip] += 1
