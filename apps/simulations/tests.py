a53ccdd2d243a85adee22085673496d659fcca62
Carol Pan <carolpan@mit.edu>
2019-04-29 16:15:53 -0400

    continuinng issues with lack of step buttons

--- apps/simulations/tests.py
+++ apps/simulations/tests.py
@@ -10,7 +10,8 @@ class SimulationsTestCase(SeleniumTestCase):
 
     def test_assembly_addition(self):
         """ Test that assembly addition simulations works and that the buttons function """
-        self.driver.get(self.live_server_url + '/simulations/assembly_additon/')
+        self.driver.get(self.live_server_url + '/simulations/assembly_addition/')
+        self.driver.implicitly_wait(5)
         self.driver.find_element_by_id('step_button').click()  # test the step button
         self.driver.find_element_by_id('reset_button').click()  # test the reset button
         self.driver.find_element_by_id('run_button').click()  # test the run button
@@ -19,6 +20,7 @@ class SimulationsTestCase(SeleniumTestCase):
     def test_floating_point_operations(self):
         """ Test that the floating point operation works and that the buttons function """
         self.driver.get(self.live_server_url + '/simulations/floating_point_operations/')
+        self.driver.implicitly_wait(5)
         self.driver.find_element_by_id('step_button').click()  # test the step button
         self.driver.find_element_by_id('reset_button').click()  # test the reset button
         self.driver.find_element_by_id('run_button').click()  # test the run button
@@ -27,6 +29,7 @@ class SimulationsTestCase(SeleniumTestCase):
     def test_looping_with_tix(self):
         """ Test that the looping with tix works and that the buttons function """
         self.driver.get(self.live_server_url + '/simulations/looping_with_tix/')
+        self.driver.implicitly_wait(5)
         self.driver.find_element_by_id('step_button').click()  # test the step button
         self.driver.find_element_by_id('reset_button').click()  # test the reset button
         self.driver.find_element_by_id('run_button').click()  # test the run button
@@ -34,8 +37,8 @@ class SimulationsTestCase(SeleniumTestCase):
 
     def test_general_assembler(self):
         """ Test that the general assembler works and that the buttons function """
-        self.driver.get(self.live_server_url + '/simulations/looping_with_tix/')
-        self.driver.find_element_by_id('code_box').send_keys("ADD 5, 1") # fill in code box
+        self.driver.get(self.live_server_url + '/simulations/general_assembler')
+        self.driver.find_element_by_id('code_box').send_keys("ADD 5, 1")  # fill in code box
         self.driver.find_element_by_id('assemble_button').click()  # assemble code
         self.driver.find_element_by_id('step_button').click()  # test the step button
         self.driver.find_element_by_id('run_button').click()  # test the run button
