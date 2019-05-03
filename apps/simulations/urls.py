6057c1c67b610797d0c610f9576fdecf953890ca
Ryaan Ahmed <ryaan.ahmed@gmail.com>
2019-05-03 16:37:42 +0100

    add trailing slash to simulations urlpatterns

--- apps/simulations/urls.py
+++ apps/simulations/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:simulation_name>', views.simulation, name="simulation"),
+    path('<str:simulation_name>/', views.simulation, name="simulation"),
     path('', views.index_sim, name='index_sim')
 ]
