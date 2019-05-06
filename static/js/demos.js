f6743c3dac3d4ccf7ab80076327f837de9ffdc9b
Mingfei <mingfeid@mit.edu>
2019-05-06 14:33:16 -0400

    Clean up arguments

--- static/js/demos.js
+++ static/js/demos.js
@@ -10,17 +10,21 @@ export const assembly_addition_demo_params = {
     initial_memory_values: [[4, 12], [5, 30]],
     instructions: [
         new Assembly_Line(
-            "CLA 4".split(" "), "<b>CLA 4: </b> Clears the accumulator and stores the value at address 4 (which" +
-            " you'll see if you mouseover is 12) into accumulator."
+            "CLA 4".split(" "),
+            "<b>CLA 4: </b> Clears the accumulator and stores the value at address 4 (which you'll see if you" +
+            " mouseover is 12) into accumulator."
         ),
         new Assembly_Line(
-            "ADD 5".split(" "), "<b>ADD 5: </b> Adds the value at address 5 (30) to the accumulator."
+            "ADD 5".split(" "),
+            "<b>ADD 5: </b> Adds the value at address 5 (30) to the accumulator."
         ),
         new Assembly_Line(
-            "STO 6".split(" "), "<b>STO 6: </b> Stores the value of the accumulator into the register at address 6."
+            "STO 6".split(" "),
+            "<b>STO 6: </b> Stores the value of the accumulator into the register at address 6."
         ),
         new Assembly_Line(
-            ["HTR", ""], "<b>HTR: </b> Tells the computer to stop."
+            ["HTR", ""],
+            "<b>HTR: </b> Tells the computer to stop."
         ),
     ],
 };
@@ -32,23 +36,29 @@ export const floating_point_operations_demo_params = {
     initial_memory_values: [],
     instructions: [
         new Assembly_Line(
-            'CLA 10'.split(" "),"<b>CLA 10</b>: Clear the accumulator and add the contents of register 10 to it."
+            'CLA 10'.split(" "),
+            "<b>CLA 10</b>: Clear the accumulator and add the contents of register 10 to it."
         ),
         new Assembly_Line(
-            'FAD 11'.split(" "), "<b>FAD 11</b>: Perform floating point addition with the accumulator and " +
-            "register 11, and store the result in the accumulator."
+            'FAD 11'.split(" "),
+            "<b>FAD 11</b>: Perform floating point addition with the accumulator and register 11, and store the" +
+            " result in the accumulator."
         ),
         new Assembly_Line(
-            'STO 12'.split(" "), "<b>STO 12</b>: Store the contents of the accumulator into register 12."
+            'STO 12'.split(" "),
+            "<b>STO 12</b>: Store the contents of the accumulator into register 12."
         ),
         new Assembly_Line(
-            'ORG 10'.split(" "), ""
+            'ORG 10'.split(" "),
+            ""
         ),
         new Assembly_Line(
-            'DEC -2.54'.split(" "), ""
+            'DEC -2.54'.split(" "),
+            ""
         ),
         new Assembly_Line(
-            'DEC 6.98'.split(" "), ""
+            'DEC 6.98'.split(" "),
+            ""
         ),
     ],
 };
@@ -59,19 +69,32 @@ export const looping_with_tix_demo_params = {
     highlighted_registers: [7],
     initial_memory_values: [[7, 1]],
     instructions: [
-        new Assembly_Line(['LXA', 'EXPONENT, 1'], "This example is relatively complicated, so you might want to read" +
-            " the long description at the bottom first.<br />" +
-        "<code>LXA EXPONENT, 1: </code> Load the word at address EXPONENT (which corresponds to PZE 5) and store its " +
-            "address into the index register corresponding to 1, which is index register A."),
-        new Assembly_Line('CLA 7'.split(" "), "<code>CLA 7: </code> Stores the value at address 7 into the accumulator."),
-        new Assembly_Line(["LOOP", "ADD", "7"], "<code>ADD 7: </code> Adds the value at address 7 to the" +
-            " accumulator."),
-        new Assembly_Line("STO 7".split(" "), "<code>STO 7: </code> Stores the value of the accumulator to address 7.  What's the value of address 7 after you" +
-        " press step?"),
-        new Assembly_Line(['TIX', 'LOOP, 1, 1'], "<code>TIX LOOP, 1, 1: </code> The computer will compare the value" +
-            " of index register A to the decrement, 1.  If the" +
-        " index register is greater than the decrement, then we subtract the decrement from the index register and jump\n" +
-        "    to the general memory 1, otherwise we continue.  What's the value of index register A after you press Step?"),
+        new Assembly_Line(
+            ['LXA', 'EXPONENT, 1'],
+            "This example is relatively complicated, so you might want to read the long description at the bottom" +
+            " first.<br /> <code>LXA EXPONENT, 1: </code> Load the word at address EXPONENT (which corresponds to" +
+            " PZE 5) and store its address into the index register corresponding to 1, which is index register A."
+        ),
+        new Assembly_Line(
+            'CLA 7'.split(" "),
+            "<code>CLA 7: </code> Stores the value at address 7 into the accumulator."
+        ),
+        new Assembly_Line(
+            ["LOOP", "ADD", "7"],
+            "<code>ADD 7: </code> Adds the value at address 7 to the accumulator."
+        ),
+        new Assembly_Line(
+            "STO 7".split(" "),
+            "<code>STO 7: </code> Stores the value of the accumulator to address 7.  What's the value of address 7" +
+            " after you press step?"
+        ),
+        new Assembly_Line(
+            ['TIX', 'LOOP, 1, 1'],
+            "<code>TIX LOOP, 1, 1: </code> The computer will compare the value of index register A to the decrement," +
+            " 1.  If the index register is greater than the decrement, then we subtract the decrement from the index" +
+            " register and jump to the general memory 1, otherwise we continue.  What's the value of index" +
+            " register A after you press Step?"
+        ),
         new Assembly_Line(["EXPONENT", "PZE", "5"]),
     ],
 };
