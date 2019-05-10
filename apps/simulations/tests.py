3e69e80c0d7d9d3607148a32c8eabb19dd28691c
Mingfei <mingfeid@mit.edu>
2019-05-10 17:07:01 -0400

    Revert "Update Travis tests"
    This reverts commit ed4a02bd81fea4fa0952103befbe4cf33ac4934b.

--- apps/simulations/tests.py
+++ apps/simulations/tests.py
@@ -10,11 +10,6 @@ class SimulationsTestCase(SeleniumTestCase):
 
     def test_assembly_addition(self):
         """ Test that assembly addition simulations works and that the buttons function """
-        for i in range(12):
-            self.driver.find_element_by_class_name('introjs-nextbutton').click()
-        for i in range(12):
-            self.driver.find_element_by_class_name('introjs-prevbutton').click()
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
         self.driver.get(self.live_server_url + '/simulations/assembly_addition/')
         self.driver.implicitly_wait(5)
         self.driver.find_element_by_id('step_button').click()  # test the step button
@@ -24,11 +19,6 @@ class SimulationsTestCase(SeleniumTestCase):
 
     def test_floating_point_operations(self):
         """ Test that the floating point operation works and that the buttons function """
-        for i in range(12):
-            self.driver.find_element_by_class_name('introjs-nextbutton').click()
-        for i in range(12):
-            self.driver.find_element_by_class_name('introjs-prevbutton').click()
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
         self.driver.get(self.live_server_url + '/simulations/floating_point_operations/')
         self.driver.implicitly_wait(5)
         self.driver.find_element_by_id('step_button').click()  # test the step button
@@ -38,11 +28,6 @@ class SimulationsTestCase(SeleniumTestCase):
 
     def test_looping_with_tix(self):
         """ Test that the looping with tix works and that the buttons function """
-        for i in range(12):
-            self.driver.find_element_by_class_name('introjs-nextbutton').click()
-        for i in range(12):
-            self.driver.find_element_by_class_name('introjs-prevbutton').click()
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
         self.driver.get(self.live_server_url + '/simulations/looping_with_tix/')
         self.driver.implicitly_wait(5)
         self.driver.find_element_by_id('step_button').click()  # test the step button
@@ -52,11 +37,6 @@ class SimulationsTestCase(SeleniumTestCase):
 
     def test_general_assembler(self):
         """ Test that the general assembler works and that the buttons function """
-        for i in range(14):
-            self.driver.find_element_by_class_name('introjs-nextbutton').click()
-        for i in range(14):
-            self.driver.find_element_by_class_name('introjs-prevbutton').click()
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
         self.driver.get(self.live_server_url + '/simulations/general_assembler')
         self.driver.find_element_by_id('add_line').click()  # test adding line
         self.driver.find_element_by_id('remove_line').click()  # test removing line
