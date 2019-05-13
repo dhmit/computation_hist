63866f79bd4bff9b91f49458a3295583acd9640b
Ryaan Ahmed <ryaan.ahmed@gmail.com>
2019-05-13 16:28:36 -0400

    fix selenium tests for simulation demos

--- apps/simulations/tests.py
+++ apps/simulations/tests.py
@@ -1,3 +1,8 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 from utilities.selenium import SeleniumTestCase
 
 
@@ -8,53 +13,45 @@ class SimulationsTestCase(SeleniumTestCase):
         self.driver.get(self.live_server_url + '/simulations/')
         self.check_log_for_errors()
 
-    def test_assembly_addition(self):
-        """ Test that assembly addition simulations works and that the buttons function """
-        self.driver.get(self.live_server_url + '/simulations/assembly_addition/')
-        self.driver.implicitly_wait(5)
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
-        self.driver.implicitly_wait(5)
-        self.scroll_to(self.driver.find_element_by_id('step_button'))
-        self.driver.find_element_by_id('step_button').click()  # test the step button
-        self.driver.find_element_by_id('reset_button').click()  # test the reset button
-        self.driver.find_element_by_id('run_button').click()  # test the run button
-        self.check_log_for_errors()
+    def skip_intro_js(self):
+        """ Skipping intro_js is a tricky business, as it only appears
+            the first time through one of the demos... which is why we
+            have this try/except pattern here. """
 
-    def test_floating_point_operations(self):
-        """ Test that the floating point operation works and that the buttons function """
-        self.driver.get(self.live_server_url + '/simulations/floating_point_operations/')
-        self.driver.implicitly_wait(5)
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
-        self.driver.implicitly_wait(5)
-        self.scroll_to(self.driver.find_element_by_id('step_button'))
-        self.driver.find_element_by_id('step_button').click()  # test the step button
-        self.driver.find_element_by_id('reset_button').click()  # test the reset button
-        self.driver.find_element_by_id('run_button').click()  # test the run button
-        self.check_log_for_errors()
+        wait = WebDriverWait(self.driver, 2)
+        try:
+            el = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'introjs-skipbutton')))
+        except TimeoutException:
+            # introjs already ran on a different demo, so it won't show up
+            return
 
-    def test_looping_with_tix(self):
-        """ Test that the looping with tix works and that the buttons function """
-        self.driver.get(self.live_server_url + '/simulations/looping_with_tix/')
-        self.driver.implicitly_wait(5)
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
-        self.driver.implicitly_wait(5)
-        self.scroll_to(self.driver.find_element_by_id('step_button'))
-        self.driver.find_element_by_id('step_button').click()  # test the step button
-        self.driver.find_element_by_id('reset_button').click()  # test the reset button
-        self.driver.find_element_by_id('run_button').click()  # test the run button
-        self.check_log_for_errors()
+        el.click()
+        overlay = self.driver.find_element_by_class_name('introjs-overlay')
+        wait.until(EC.staleness_of(overlay))
+
+    def test_simulation_demos(self):
+        """ Test that the simulation demos work and that the buttons function """
+        demos = ['floating_point_operations',
+                 'assembly_addition',
+                 'looping_with_tix']
+        for i, demo in enumerate(demos):
+            self.driver.get(self.live_server_url + '/simulations/' + demo)
+            self.skip_intro_js()
+            self.driver.find_element_by_id('step_button').click()  # test the step button
+            self.driver.find_element_by_id('reset_button').click()  # test the reset button
+            self.driver.find_element_by_id('run_button').click()  # test the run button
+            self.check_log_for_errors()
 
     def test_general_assembler(self):
         """ Test that the general assembler works and that the buttons function """
         self.driver.get(self.live_server_url + '/simulations/general_assembler')
-        self.driver.implicitly_wait(7)
-        self.driver.find_element_by_class_name('introjs-skipbutton').click()
-        self.driver.implicitly_wait(5)
-        self.scroll_to(self.driver.find_element_by_id('add_line'))
+
+        self.skip_intro_js()
+
         self.driver.find_element_by_id('add_line').click()  # test adding line
         self.driver.find_element_by_id('remove_line').click()  # test removing line
+
         # fill in code boxes
-        self.scroll_to(self.driver.find_element_by_class_name('code_label'))
         self.driver.find_element_by_class_name('code_label').send_keys("LABEL")
         self.driver.find_element_by_class_name('code_operation').send_keys("ADD")
         self.driver.find_element_by_class_name('code_numbers').send_keys("LABEL+1, 3")
