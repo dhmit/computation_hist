34fd067e45b4340c0e77070dd7b220ac1c944a57
Ryaan Ahmed <ryaan.ahmed@gmail.com>
2019-05-17 10:07:56 -0400

    added missing comma in list of stories

--- apps/archives/views.py
+++ apps/archives/views.py
@@ -10,7 +10,7 @@ from utilities.common import load_pickle
 
 
 STORIES = [
-    'about_us_story'  
+    'about_us_story',
     'announcement_of_the_IBM_704',
     'beginnings_of_cs_at_mit',
     'cost_of_using_a_supercomputer',
